77ac508a0c5f8709b65416bcf70359d34e1326d4
julian <pedrazajulian@hotmail.com>
2021-09-23 21:59:08 -0400

    reworked on files

---
+++ src/resumeData.js
@@ -0,0 +1,117 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Julian Pedraza",
+    "role": "Frontend Developer || Human Resources Profesional",
+    "linkedinId":"https://www.linkedin.com/in/julian-pedraza-299875180/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/julian-pedraza-299875180/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/pedrazajulian",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"404",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Human Resources professional wanting to learn more about the backend of the websites that we use on a daily basis, I decided to start taking a Full stack web Developer with enphasis in: Computer Science applied to JavaScript, Browser Based Technologies (HTML, CSS,JavaScript, jQuery), Deployment, Java, Server Side Development (Node.js).My Goal is to be able to help business and individuals grow with modern Technologies by helping them navigate todays complex internet competition.",
+    "address":"1234 paper street",
+    "website":"https://pedrazajulian.github.io/Portfolio/",
+    "education":[
+      {
+        "UniversityName":"UNCC",
+        "specialization":"Fullstack web Developing",
+        "MonthOfPassing":"Oct",
+        "YearOfPassing":"2021",
+        "Achievements":"Portfolio"
+      },
+      {
+        "UniversityName":"University of San Diego",
+        "specialization":"Osha 10",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Industrial Safety Diploma"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Productiv Inc.",
+        "specialization":"HR Manager",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"-",
+        "Achievements":"Implementing HR policies and procedures"
+      },
+      {
+        "CompanyName":"Monroe Staffing",
+        "specialization":"Onsite Manager",
+        "MonthOfLeaving":"Sept",
+        "YearOfLeaving":"2020",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Special Project Management",
+        "description":"API Integration/HTML/CSS/JS",
+        "imgurl":"images/portfolio/project1.jpg",
+        "url":"https://tloyzelle.github.io/Project-Marvel/"
+      },
+      {
+        "name":"Cyber-Security",
+        "description":"Secure Password Generation/Web Security",
+        "imgurl":"images/portfolio/project1.jpg",
+        "url":"https://pedrazajulian.github.io/PasswordGenerator/"
+      },
+      {
+        "name":"pCDP Systems",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png",
+        "url": "https://protected-gorge-09109.herokuapp.com/"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This guy is the best thing since Coca-Cola",
+        "name":"Tyler Durden"
+      },
+      {
+        "description":"Julian Save my life",
+        "name":"Barack Obama"
+      },
+      {
+        "description":"Julian Save My business",
+        "name":"George W Bush"
+      },
+      {
+        "description":"I trained Julian for many Years",
+        "name":"Bruce Lee"
+      }
+    ]
+  }
+  
+  export default resumeData
